e0c159fcc7d721d3846d7122324991af7fc57d77
fsbs2 <gabriel_fsbs2@hotmail.com>
2022-09-17 23:59:37 -0300

    OpenApi spec 3.0 Swagger with codegen plugin

---
+++ src/main/java/com/devfortech/pequenosgenios/PequenosGeniosApplication.java
@@ -0,0 +1,12 @@
+package com.devfortech.pequenosgenios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PequenosGeniosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PequenosGeniosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devfortech/pequenosgenios/PequenosGeniosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfortech.pequenosgenios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PequenosGeniosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
