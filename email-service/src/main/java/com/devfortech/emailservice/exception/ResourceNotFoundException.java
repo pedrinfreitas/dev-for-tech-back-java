68f7436a0158e424dd9fe90867d9269fba5defff
black727 <jefersonsantos006@hotmail.com>
2022-09-19 14:37:37 -0300

    feat: Criação do micro servico de envio de emails

---
+++ email-service/src/main/java/com/devfortech/emailservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.devfortech.emailservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 7357988091776452873L;
+
+    public ResourceNotFoundException(String ex){
+        super(ex);
+    }
+}
---
+++ email-service/src/main/java/com/devfortech/emailservice/service/EmailService.java
@@ -0,0 +1,49 @@
+package com.devfortech.emailservice.service;
+
+import com.devfortech.emailservice.domain.entity.EmailModel;
+import com.devfortech.emailservice.domain.enums.StatusEnvio;
+import com.devfortech.emailservice.domain.repository.EmailModelRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final EmailModelRepository emailRepository;
+    private final JavaMailSender emailSender;
+
+    public void sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEnvio(StatusEnvio.SENT);
+        } catch (MailException e){
+            emailModel.setStatusEnvio(StatusEnvio.ERROR);
+        } finally {
+            emailRepository.save(emailModel);
+        }
+    }
+
+    public Page<EmailModel> findAll(Pageable pageable) {
+        return  emailRepository.findAll(pageable);
+    }
+
+    public Optional<EmailModel> findById(Long id) {
+        return emailRepository.findById(id);
+    }
+}
