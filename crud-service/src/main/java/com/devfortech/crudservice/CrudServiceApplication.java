6f5d514398329f078d05f6c2081a46f3e661d66e
fsbs2 <gabriel_fsbs2@hotmail.com>
2022-09-23 03:25:51 -0300

    feat: Adiciona dependências para gerar documentação

--- crud-service/src/main/java/com/devfortech/crudservice/CrudServiceApplication.java
+++ crud-service/src/main/java/com/devfortech/crudservice/CrudServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@OpenAPIDefinition
 public class CrudServiceApplication {
 
     public static void main(String[] args) {
